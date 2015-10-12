d4e8076b251503a652b4ae604b45d52bca402ba9
vsharonlynn <v.sharon.lynn@gmail.com>
2015-10-12 22:13:39 +0800

    Added installed apps and timezone false

--- api/cvia/settings.py
+++ api/cvia/settings.py
@@ -25,12 +25,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'cvia',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -90,7 +94,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
