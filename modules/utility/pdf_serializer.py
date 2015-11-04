5c2dd4e046e677b5b259ca3b7bd068a18609ea24
BenedictSu <Benedict_S_S@u.nus.edu>
2015-11-04 16:10:54 +0800

    added pdf converter version 1
    using PyPDF2 but it does not convert correctly yet

--- modules/utility/pdf_serializer.py
+++ modules/utility/pdf_serializer.py
@@ -1,10 +1,25 @@
 '''
 PDF read method here
 '''
+from PyPDF2 import PdfFileReader
+
 class PdfSerializer(object):
     def __init__(self, filename):
         self.filename = filename
 
     def getText(self):
         '''Convert PDF to text'''
+        pdfFile = PdfFileReader(open(self.filename, "rb"))
+        pages = pdfFile.getNumPages()
+        txtFile = open(self.filename.replace(".pdf", ".txt"), "w")
+
+        for x in range(0, pages):
+            page = pdfFile.getPage(x).extractText()
+            txtFile.write(page.encode('ascii','ignore').decode("utf-8") )
+
+        txtFile.close()
+        
         pass
+
+convert = PdfSerializer("t.pdf")
+convert.getText()
