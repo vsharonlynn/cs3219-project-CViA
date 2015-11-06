330e209d3eb0e8fc3c6c1b13eb4418717324099a
Evan Sebastian <evanlhoini@gmail.com>
2015-11-06 21:15:36 +0800

    Modify settings and .gitignore

--- api/cvia/settings.py
+++ api/cvia/settings.py
@@ -19,8 +19,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
-ALLOWED_HOSTS = []
+DEBUG = True
 
+ALLOWED_HOSTS = []
 
 # Application definition
 
