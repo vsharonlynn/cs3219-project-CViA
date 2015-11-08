95006a5dac12e51df4a87473abe6c544667ee5dc
vsharonlynn <v.sharon.lynn@gmail.com>
2015-11-08 17:41:43 +0800

    Added some code into parser

--- modules/parser.py
+++ modules/parser.py
@@ -4,7 +4,7 @@ Converts PDF file to data structure
 class Parser(object):
     def __init__(self, filename):
         self.filename = filename
-        # initialize self.categories list
+        self.categories = ['education', 'work experience', 'skills', 'activities']
 
     def parse(self):
         #extractPDF
@@ -16,6 +16,10 @@ class Parser(object):
 
     def generateDataStructure(self):
         # initilize all categories list to []
+        data_structure = {}
+        for category in categories:
+            data_structure[category] = []
+
         # for each section, get base category, append members to category
-        # to come up with [{category: [member, ...]}, {category: [member, ...]}, ...]
+        # to come up with {category: [member, ...], category: [member, ...], ...}
         pass
