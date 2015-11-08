dd553df13179b29db3a1fa9550dfb4c68c38be0d
tanch88 <chunhow88@hotmail.com>
2015-11-08 20:47:42 +0800

    solve null pointer for determineSynonym

--- modules/utility/synonym.py
+++ modules/utility/synonym.py
@@ -12,11 +12,13 @@ class Synonym():
         return set(synonyms)
 
     def determineSynonym(first, second):
-        w1 = wordnet.synset(wordnet.synsets(first)[0].name())
-        w2 = wordnet.synset(wordnet.synsets(second)[0].name())
+        if len(wordnet.synsets(first)) != 0 & len(wordnet.synsets(second)) != 0:
+            w1 = wordnet.synset(wordnet.synsets(first)[0].name())
+            w2 = wordnet.synset(wordnet.synsets(second)[0].name())
+            return w1.wup_similarity(w2)
+        else:
+            return 0
 
-        return w1.wup_similarity(w2)
 
-
-# print(Synonym.determineSynonym("studies", "education"))
+#print(Synonym.determineSynonym("edu", "education"))
 # print(Synonym.determineSynonym("school", "education"))
