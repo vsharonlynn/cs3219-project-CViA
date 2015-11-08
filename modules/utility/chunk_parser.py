aa31f3764603a77115e869fea373cf6cd860a27d
tanch88 <chunhow88@hotmail.com>
2015-11-08 22:44:00 +0800

    implement chunk_parser with backoff

--- modules/utility/chunk_parser.py
+++ modules/utility/chunk_parser.py
@@ -29,7 +29,10 @@ Research Intern Genome Institute of Singapore. A*STAR, Singapore Aug – Oct 201
 class ChunkParser(nltk.ChunkParserI):
     def __init__(self, train_sents):
          train_data = ChunkParser.tag(train_sents)
-         self.tagger = nltk.UnigramTagger(train_data)
+         t0 = nltk.DefaultTagger('NN')
+         t1 = nltk.UnigramTagger(train_data, backoff=t0)
+         t2 = nltk.BigramTagger(train_data, backoff=t1)
+         self.tagger = t2
 
     def parse(self, test_sents):
          test_data = ChunkParser.tokenize(test_sents)
@@ -54,6 +57,6 @@ class ChunkParser(nltk.ChunkParserI):
                     list.append(pos.encode('ascii', 'ignore').decode('utf-8'))
         return list
 
-# unigram_tagger = ChunkParser(cv1)
-# tagged_data = unigram_tagger.parse(cv2);
-# print(ChunkParser.extract(tagged_data))
+#unigram_tagger = ChunkParser(cv1)
+#tagged_data = unigram_tagger.parse(cv2)
+#print(ChunkParser.extract(tagged_data))
