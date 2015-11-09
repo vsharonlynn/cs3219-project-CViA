510b37f20d417ead2ad16ae81d4750519ceffb76
vsharonlynn <v.sharon.lynn@gmail.com>
2015-11-09 21:53:42 +0800

    Added private variables to analyser

--- modules/analyser.py
+++ modules/analyser.py
@@ -3,37 +3,37 @@ Calculates the score between two data structures
 '''
 class Analyser(object):
     def __init__(self, jd):
-        self.score = 0
-        self.jd = jd
-        self.fields = ["education", "skills", "experience", "activities"]
-        self.weightage = [0.25, 0.25, 0.25, 0.25]
+        self.__score = 0
+        self.__jd = jd
+        self.__fields = ["education", "skills", "experience", "activities"]
+        self.__weightage = [0.25, 0.25, 0.25, 0.25]
 
     def compare(self, resume):
         # compare jd and resume while updating intemediate score and JD counts
         self.intScore = [0, 0, 0, 0]
         self.jdTotal = [0, 0, 0, 0]
         for i in range(0, 4):
-            if ((self.fields[i] in jd) and (self.fields[i] in resume)):
-                for key in self.jd.get(self.fields[i]):
+            if ((self.__fields[i] in jd) and (self.__fields[i] in resume)):
+                for key in self.__jd.get(self.__fields[i]):
                     self.jdTotal[i] += 1
-                    if (resume.get(self.fields[i]).count(key) != 0):
+                    if (resume.get(self.__fields[i]).count(key) != 0):
                         self.intScore[i] += 1
 
     def changeWeightage(self, edu, skill, exp, act):
         # allow user to update the weigtage for counting of scores
-        self.weightage[0] = edu
-        self.weightage[1] = skill
-        self.weightage[2] = exp
-        self.weightage[3] = act
+        self.__weightage[0] = edu
+        self.__weightage[1] = skill
+        self.__weightage[2] = exp
+        self.__weightage[3] = act
 
     def computeScore(self):
         # update the score using the weightage given
         for i in range(0, 4):
             if self.intScore[i] != 0:
-                self.score += self.weightage[i] * self.intScore[i]/self.jdTotal[i]
+                self.__score += self.__weightage[i] * self.intScore[i]/self.jdTotal[i]
 
     def getScore(self):
-        return self.score
+        return self.__score
 
 # jd = {"education": ["Singapore", "NUS", "Oxford"],
 #      "skills": ["python", "java"],
