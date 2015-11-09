ab77e77160d1e8cc89dc76ebbce4a5e8359d166f
vsharonlynn <v.sharon.lynn@gmail.com>
2015-11-09 19:17:47 +0800

    Cleaned up analyser

--- modules/analyser.py
+++ modules/analyser.py
@@ -2,24 +2,21 @@
 Calculates the score between two data structures
 '''
 class Analyser(object):
-    global fields
-    fields = ["education", "skills", "experience", "activities"]
-    weightage = [0.25, 0.25, 0.25, 0.25]
-    
-    def __init__(self, jd, resume):
-        self.jd = jd
-        self.resume = resume
+    def __init__(self, jd):
         self.score = 0
+        self.jd = jd
+        self.fields = ["education", "skills", "experience", "activities"]
+        self.weightage = [0.25, 0.25, 0.25, 0.25]
 
-    def compare(self):
+    def compare(self, resume):
         # compare jd and resume while updating intemediate score and JD counts
         self.intScore = [0, 0, 0, 0]
         self.jdTotal = [0, 0, 0, 0]
         for i in range(0, 4):
-            if ((fields[i] in self.jd) and (fields[i] in self.resume)):
-                for key in self.jd.get(fields[i]):
+            if ((self.fields[i] in jd) and (self.fields[i] in resume)):
+                for key in self.jd.get(self.fields[i]):
                     self.jdTotal[i] += 1
-                    if (self.resume.get(fields[i]).count(key) != 0):
+                    if (resume.get(self.fields[i]).count(key) != 0):
                         self.intScore[i] += 1
 
     def changeWeightage(self, edu, skill, exp, act):
@@ -40,18 +37,18 @@ class Analyser(object):
     def getScore(self):
         return self.score
 
-##jd = {"education": ["Singapore", "NUS", "Oxford"],
-##      "skills": ["python", "java"],
-##      "experience": ["3 years"],
-##      "activities": ["ICPC"]}
-##cv = {"education": ["Malaysia", "NTU", "Oxford"],
-##      "skills": ["python", "javascript"],
-##      "EXPERIENCE": [],
-##      "activities": ["ACM"]}
-##test = Analyser(jd, cv)
-##test.compare()
-##test.computeScore()
-##print(test.getScore())
-##test.changeWeightage(0.1, 0.2, 0.3, 0.4)
-##test.computeScore()
-##print(test.getScore())
+# jd = {"education": ["Singapore", "NUS", "Oxford"],
+#      "skills": ["python", "java"],
+#      "experience": ["3 years"],
+#      "activities": ["ICPC"]}
+# cv = {"education": ["Malaysia", "NTU", "Oxford"],
+#      "skills": ["python", "javascript"],
+#      "EXPERIENCE": [],
+#      "activities": ["ACM"]}
+# test = Analyser(jd)
+# test.compare(cv)
+# test.computeScore()
+# print(test.getScore())
+# test.changeWeightage(0.1, 0.2, 0.3, 0.4)
+# test.computeScore()
+# print(test.getScore())
